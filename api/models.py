da4ba26c0deeeed76f4195a7315ba48304df3855
Кирилл Левин <levin_ka@devtrix.ru>
2024-06-26 20:45:48 +0300

    Initial commit. Set up project, Create GlucoseLevel model

---
+++ api/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class GlucoseLevel(models.Model):
+    TEST_TYPE_CHOICES = [
+        (0, 'Automatic transmission'),
+        (1, 'Sensor scan'),
+        (6, 'No glucose level data'),
+    ]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='glucose_levels',
+    )
+
+    device_name = models.CharField(
+        max_length=100,
+    )
+
+    device_serial_number = models.CharField(
+        max_length=36,
+    )
+
+    timestamp = models.DateTimeField()
+
+    recording_type = models.IntegerField(
+        choices=TEST_TYPE_CHOICES,
+    )
+
+    glucose_value = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Glucose Level {self.id} for User {self.user.username}"
